8c1cfddf903218038df16316e49c09912037c41c
kotobukid <kotobuki@daqua.jp>
2022-07-27 18:28:00 +0900

    rollupのオプションを調査したところ、生成ファイルの名前を固定することができた アセットファイル(ts>js以外？)の同項目の指定は別に行う必要があった模様

---
+++ out/entry-sub.js
@@ -0,0 +1,1 @@
+import{g as o}from"./as/runtime-dom.esm-bundler.1dc1c730.js";const l={hello:"hello"};console.log("aaa"),console.log(l.hello);o({}).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,21 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    build: {
-        outDir: './out',
-        assetsDir: './as',
-        rollupOptions: {
-            input: {
-                main: 'src/main.ts',
-                sub: 'src/sub.ts',
+        plugins: [vue()],
+        build: {
+            outDir: './out',
+            assetsDir: './as',
+            rollupOptions: {
+                input: {
+                    main: 'src/main.ts',
+                    sub: 'src/sub.ts',
+                    style: 'src/style.css'
+                },
+                output: {
+                    entryFileNames: 'entry-[name].js',
+                    assetFileNames: 'asset-[name].[ext]'
+                },
             }
-        },
+        }
     }
-})
+)
