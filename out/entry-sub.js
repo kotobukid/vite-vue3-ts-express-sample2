4c823e8788c061d6a421dfdeec6cb27b3c8379d8
kotobukid <kotobuki@daqua.jp>
2022-07-27 18:32:50 +0900

    script setupを使用するとこうなる

--- out/entry-sub.js
+++ out/entry-sub.js
@@ -1,1 +1,1 @@
-import{h as o}from"./as/runtime-dom.esm-bundler.11b0fa1c.js";const l={hello:"hello"};console.log("aaa"),console.log(l.hello);o({}).mount("#app");
+import{g as o}from"./as/runtime-dom.esm-bundler.e891f92d.js";const l={hello:"hello"};console.log("aaa"),console.log(l.hello);o({}).mount("#app");
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
-<script lang="ts">
-import {defineComponent} from "vue";
+<script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 
@@ -7,14 +6,6 @@ const say_hello = () => {alert('hello')}
 
 import HelloWorld from './components/HelloWorld.vue'
 
-export default defineComponent({
-    components: {
-        HelloWorld
-    },
-    methods: {
-        say_hello
-    }
-})
 </script>
 
 <template lang="pug">
